26cea979fc415c5b4f8166153c43141dd0a0ab61
hata-tomoyuki <h.tomo0602@gmail.com>
2022-08-17 07:03:39 +0900

    mod style

--- components/Services.jsx
+++ components/Services.jsx
@@ -4,14 +4,14 @@ import { FadeIn } from '../components/FadeIn';
 
 function Services({services}) {
   return (
-    <div className='p-8 lg:p-32 overflow-hidden max-w-[1440px] mx-auto'>
+    <div className='p-8 lg:p-32 overflow-hidden max-w-[1440px] mx-auto mt-32 lg:mt-0'>
       <h2 className='text-4xl sm:border-b-4 sm:border-emerald-600 w-fit font-bold mt-12 lg:mt-0'>What We Can Do?</h2>
       <h2 className='text-4xl lg:text-5xl text-emerald-700 font-bold mt-6'>Services we can help you with</h2>
-      <div className='flex flex-wrap justify-center md:justify-between mt-10 gap-20'>
+      <div className='flex flex-wrap justify-center md:justify-between mt-10 gap-10 lg:gap-20'>
         {services.map((service) => (
           <Link passHref key={service.id} href={`/products/${service.name}`}>
             <FadeIn>
-              <div className='w-[245px] h-[245px] sm:w-[500px] sm:h-[500px] flex justify-center items-center p-2 sm:p-4 bg-gray-200 mb-10 relative group rounded'>
+              <div className='w-[245px] h-[245px] sm:w-[500px] sm:h-[500px] flex justify-center items-center p-2 sm:p-4 bg-gray-200 lg:mb-10 relative group rounded'>
                 <div className='w-[70%] text-center absolute z-50 bg-gray-300/80 rounded-tr-xl p-4 opacity-0 group-hover:opacity-100 ease-in duration-300 select-none'>{service.desc}</div>
                 <span className='absolute top-4 left-4 text-gray-600 font-light tracking-widest uppercase'>{service.title}</span>
                 <div className='w-[70%] h-[70%] sm:w-[85%] sm:h-[85%] rounded-[50%] overflow-hidden group-hover:scale-110 ease-in duration-300 mt-4 lg:mt-0'>
--- pages/contact.js
+++ pages/contact.js
@@ -10,22 +10,24 @@ function contact() {
       exit={{ opacity: 0 }}    // アンマウント時
       transition={{ ease: "easeOut", duration: 0.3 }}
     >
-      <div className='flex flex-col items-center justify-center relative pb-20 lg:mb-52 overflow-hidden'>
-          <Circle backgroundColor="lightgreen" left='-40vh' top='-20vh' />
-          <Circle backgroundColor="yellow" right='-30vh' bottom='-60vh' />
-          <h2 className='text-4xl lg:text-5xl font-bold mt-32 mb-20'>
-              GET IN TOUCH
-          </h2>
-          <from className='w-11/12 h-full md:w-[500px] md:h-[500px] flex flex-wrap'>
-              <div className='md:flex gap-4 w-full md:mb-10'>
-                  <input type="text" placeholder='Name' className='w-full md:w-1/2 mb-10 md:mb-0 h-16 pl-4 border border-gray-500' />
-                  <input type="text" placeholder='Phone' className='w-full md:w-1/2 mb-10 md:mb-0 h-16 pl-4 border border-gray-500' />
-              </div>
-              <input type="text" placeholder='Email' className='w-full mb-10 h-16 pl-4 border border-gray-500' />
-              <input type="text" placeholder='Subject' className='w-full mb-10 h-16 pl-4 border border-gray-500' />
-              <textarea cols="30" rows="10" placeholder='Message' className='w-full pl-4 pt-4 border border-gray-500'></textarea>
-              <button className='mt-10 py-4 rounded bg-emerald-600 text-white w-full cursor-pointer text-xl'>SUBMIT</button>
-          </from>
+      <div className='overflow-scroll'>
+        <div className='flex flex-col items-center justify-center relative pb-20 lg:mb-52 overflow-hidden'>
+            <Circle backgroundColor="lightgreen" left='-40vh' top='-20vh' />
+            <Circle backgroundColor="yellow" right='-30vh' bottom='-60vh' />
+            <h2 className='text-4xl lg:text-5xl font-bold mt-32 mb-20'>
+                GET IN TOUCH
+            </h2>
+            <from className='w-11/12 h-full md:w-[500px] md:h-[500px] flex flex-wrap'>
+                <div className='md:flex gap-4 w-full md:mb-10'>
+                    <input type="text" placeholder='Name' className='w-full md:w-1/2 mb-10 md:mb-0 h-16 pl-4 border border-gray-500' />
+                    <input type="text" placeholder='Phone' className='w-full md:w-1/2 mb-10 md:mb-0 h-16 pl-4 border border-gray-500' />
+                </div>
+                <input type="text" placeholder='Email' className='w-full mb-10 h-16 pl-4 border border-gray-500' />
+                <input type="text" placeholder='Subject' className='w-full mb-10 h-16 pl-4 border border-gray-500' />
+                <textarea cols="30" rows="10" placeholder='Message' className='w-full pl-4 pt-4 border border-gray-500'></textarea>
+                <button className='mt-10 py-4 rounded bg-emerald-600 text-white w-full cursor-pointer text-xl'>SUBMIT</button>
+            </from>
+        </div>
       </div>
     </motion.div>
   )
