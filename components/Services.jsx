ce47ea36f85360040dab86a4a1ddab850fe360b3
hata-tomoyuki <58424529+hata-tomoyuki@users.noreply.github.com>
2022-08-17 08:57:13 +0900

    Add fadein animation (#1)
    * revert jsx file for fadein
    
    * apply fadein animation

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,8 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { FadeIn } from '../components/FadeIn';
+import { scrollFadeIn } from './scrollFadein';
+import { useEffect } from 'react';
 
 function Services({services}) {
+
+  useEffect(() => {
+    scrollFadeIn();
+  }, []);
+
   return (
     <div className='p-8 lg:p-32 overflow-hidden max-w-[1440px] mx-auto lg:mt-0'>
       <h2 className='text-4xl sm:border-b-4 sm:border-emerald-600 w-fit font-bold mt-12 lg:mt-0'>What We Can Do?</h2>
@@ -11,7 +17,7 @@ function Services({services}) {
         {services.map((service) => (
           <Link passHref key={service.id} href={`/products/${service.name}`}>
             {/* <FadeIn> */}
-              <div className='w-[245px] h-[245px] sm:w-[500px] sm:h-[500px] flex justify-center items-center p-2 sm:p-4 bg-gray-200 lg:mb-10 relative group rounded'>
+              <div className='w-[245px] h-[245px] sm:w-[500px] sm:h-[500px] flex justify-center items-center p-2 sm:p-4 bg-gray-200 lg:mb-10 relative group rounded js-show-on-scroll'>
                 <div className='w-[70%] text-center absolute z-50 bg-gray-300/80 rounded-tr-xl p-4 opacity-0 group-hover:opacity-100 ease-in duration-300 select-none'>{service.desc}</div>
                 <span className='absolute top-4 left-4 text-gray-600 font-light tracking-widest uppercase'>{service.title}</span>
                 <div className='w-[70%] h-[70%] sm:w-[85%] sm:h-[85%] rounded-[50%] overflow-hidden group-hover:scale-110 ease-in duration-300 mt-4 lg:mt-0'>
