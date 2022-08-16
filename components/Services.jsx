1900c6d08fab95f08217ec15772c7cbeb12dc0ea
hata-tomoyuki <h.tomo0602@gmail.com>
2022-08-16 22:59:07 +0900

    fadein animation by react-intersection-observer

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,13 +1,8 @@
 import Link from 'next/link'
-import React, { useEffect } from 'react'
 import Image from 'next/image'
-import { scrollFadeIn } from './scrollFadein'
+import { FadeIn } from '../components/FadeIn';
 
 function Services({services}) {
-  useEffect(() => {
-    scrollFadeIn();
-  }, []);
-
   return (
     <div className='p-8 lg:p-32 overflow-hidden max-w-[1440px] mx-auto'>
       <h2 className='text-4xl sm:border-b-4 sm:border-emerald-600 w-fit font-bold mt-12 lg:mt-0'>What We Can Do?</h2>
@@ -15,30 +10,32 @@ function Services({services}) {
       <div className='flex flex-wrap justify-center md:justify-between mt-10 gap-20'>
         {services.map((service) => (
           <Link passHref key={service.id} href={`/products/${service.name}`}>
-            <div className='w-[245px] h-[245px] sm:w-[500px] sm:h-[500px] flex justify-center items-center p-2 sm:p-4 bg-gray-200 mb-10 relative group rounded js-show-on-scroll'>
-              <div className='w-[70%] text-center absolute z-50 bg-gray-300/80 rounded-tr-xl p-4 opacity-0 group-hover:opacity-100 ease-in duration-300 select-none'>{service.desc}</div>
-              <span className='absolute top-4 left-4 text-gray-600 font-light tracking-widest uppercase'>{service.title}</span>
-              <div className='w-[70%] h-[70%] sm:w-[85%] sm:h-[85%] rounded-[50%] overflow-hidden group-hover:scale-110 ease-in duration-300 mt-4 lg:mt-0'>
-                {service.video ? (
-                  <video 
-                    className='w-full h-full object-cover' 
-                    src={`/img/${service.video}`} 
-                    autoPlay 
-                    loop 
-                    muted 
-                  />
-                ) : (
-                  <Image 
-                    src={`/img/${service.photo}`} 
-                    alt='Image'
-                    width='100%'
-                    height='100%'
-                    layout='responsive'
-                    objectFit='cover'
-                  />
-                )}
+            <FadeIn>
+              <div className='w-[245px] h-[245px] sm:w-[500px] sm:h-[500px] flex justify-center items-center p-2 sm:p-4 bg-gray-200 mb-10 relative group rounded'>
+                <div className='w-[70%] text-center absolute z-50 bg-gray-300/80 rounded-tr-xl p-4 opacity-0 group-hover:opacity-100 ease-in duration-300 select-none'>{service.desc}</div>
+                <span className='absolute top-4 left-4 text-gray-600 font-light tracking-widest uppercase'>{service.title}</span>
+                <div className='w-[70%] h-[70%] sm:w-[85%] sm:h-[85%] rounded-[50%] overflow-hidden group-hover:scale-110 ease-in duration-300 mt-4 lg:mt-0'>
+                  {service.video ? (
+                    <video 
+                      className='w-full h-full object-cover' 
+                      src={`/img/${service.video}`} 
+                      autoPlay 
+                      loop 
+                      muted 
+                    />
+                  ) : (
+                    <Image 
+                      src={`/img/${service.photo}`} 
+                      alt='Image'
+                      width='100%'
+                      height='100%'
+                      layout='responsive'
+                      objectFit='cover'
+                    />
+                  )}
+                </div>
               </div>
-            </div>
+            </FadeIn>
           </Link>
         ))}
       </div>
--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,47 +1,44 @@
-import React, { useEffect } from 'react'
 import Circle from './Circle'
 import {users} from '../data'
 import Image from 'next/image'
-import { scrollFadeIn } from './scrollFadein'
+import { FadeIn } from '../components/FadeIn';
 
 function Testimonials() {
-    useEffect(() => {
-        scrollFadeIn();
-    }, []);
-
   return (
     <div className='relative flex flex-col items-center justify-around my-12 lg:my-32 max-w-[1280px] mx-auto'>
         <Circle backgroundColor="lightgreen" top="-70vh" left="0" right="0" />
         <h2 className='text-3xl lg:text-5xl font-bold text-center mb-2 lg:mb-28'>Testimonials</h2>
-        <div className='flex flex-wrap items-center justify-around flex-col lg:flex-row js-show-on-scroll'>
-            {users.map(user => (
-                <div key={user.id} className='w-[30%] my-10 lg:my-0'>
-                    <Image
-                        src={`/img/${user.logo}`}
-                        alt='Image'
-                        width='30'
-                        height='30'
-                    />
-                    <p className='text-xl font-light break-words my-4'>
-                        {user.comment}
-                    </p>
-                    <div className='flex items-center'>
+        <FadeIn>
+            <div className='flex flex-wrap items-center justify-around flex-col lg:flex-row'>
+                {users.map(user => (
+                    <div key={user.id} className='w-[30%] my-10 lg:my-0'>
                         <Image
-                            className='rounded-[50%]'
-                            src={`/img/${user.avater}`}
+                            src={`/img/${user.logo}`}
                             alt='Image'
-                            width='45'
-                            height='45'
-                            objectFit='cover'
+                            width='30'
+                            height='30'
                         />
-                        <div className='flex flex-col ml-4'>
-                            <span className='font-bold text-gray-600'>{user.name}</span>
-                            <span className='text-sm'>{user.title}</span>
+                        <p className='text-xl font-light break-words my-4'>
+                            {user.comment}
+                        </p>
+                        <div className='flex items-center'>
+                            <Image
+                                className='rounded-[50%]'
+                                src={`/img/${user.avater}`}
+                                alt='Image'
+                                width='45'
+                                height='45'
+                                objectFit='cover'
+                            />
+                            <div className='flex flex-col ml-4'>
+                                <span className='font-bold text-gray-600'>{user.name}</span>
+                                <span className='text-sm'>{user.title}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </FadeIn>
     </div>
   )
 }
